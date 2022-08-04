6194105c36e1960bc5a2b354ee5587d176e03627
JONGWON <tltlxl92@naver.com>
2022-08-04 16:51:15 +0900

    수정중

--- MangoPlate/MangoWeb/member/views.py
+++ MangoPlate/MangoWeb/member/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponseRedirect
 from .forms import RegisterForm
 from django.contrib import auth
 
+def login(request):
+    return render(request, "login.html")
 
 def register(request):
     if request.method == "POST":
@@ -10,8 +12,10 @@ def register(request):
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             pwd = form.cleaned_data['pwd']
-            print(name, email, pwd)
-            return HttpResponseRedirect('/')
+            pwd2 = request.POST["password2"]
+            if pwd == pwd2:
+                print(name, email, pwd)
+                return HttpResponseRedirect('/')
     else:
         form = RegisterForm()
     return render(request, "register.html", {'form': form})
