d379c53e8cc1d6f8afa93a315d3c09e0a1fdd8c4
bij01 <51735776+bij01@users.noreply.github.com>
2022-08-24 17:18:57 +0900

    Delete MangoPlate/YSH/MangoPlate/YSH/MangoPlate directory

--- MangoPlate/YSH/MangoPlate/YSH/MangoPlate/YSH/MangoPlate/MangoWeb/member/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class RegisterForm(forms.Form):
-    name = forms.CharField(label="name", max_length=30)
-    email = forms.EmailField(label="email", max_length=60)
-    pwd = forms.CharField(max_length=45)
